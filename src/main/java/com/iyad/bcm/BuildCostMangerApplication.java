16500e17dba9c13fca39dd23387654e21bd56bc7
IyadA <iyada@radware.com>
2025-01-31 19:24:12 +0200

    create the project

---
+++ src/main/java/com/iyad/bcm/BuildCostMangerApplication.java
@@ -0,0 +1,13 @@
+package com.iyad.bcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildCostMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildCostMangerApplication.class, args);
+	}
+
+}
