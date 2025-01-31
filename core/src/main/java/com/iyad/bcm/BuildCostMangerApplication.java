b6949a1415896591ae998a8f097005bffd9892e8
IyadA <iyada@radware.com>
2025-01-31 22:59:26 +0200

    use the database module

--- core/src/main/java/com/iyad/bcm/BuildCostMangerApplication.java
+++ core/src/main/java/com/iyad/bcm/BuildCostMangerApplication.java
@@ -2,8 +2,12 @@ package com.iyad.bcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.iyad.model")
+@EnableJpaRepositories("com.iyad.repository")
 public class BuildCostMangerApplication {
 
 	public static void main(String[] args) {
