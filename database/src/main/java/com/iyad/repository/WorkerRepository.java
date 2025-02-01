d241199452381452cfe7a1556e8123815c17a4ae
IyadA <iyada@radware.com>
2025-02-01 21:19:00 +0200

    add new method to findByName

--- database/src/main/java/com/iyad/repository/WorkerRepository.java
+++ database/src/main/java/com/iyad/repository/WorkerRepository.java
@@ -4,7 +4,9 @@ import com.iyad.model.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface WorkerRepository extends JpaRepository<Worker, UUID> {
+    Optional findByName(String workerName);
 }
