ccbee2b2eea095c33cc291928c2a496bde665c20
IyadA <iyada@radware.com>
2025-02-01 14:54:16 +0200

    adding more tables and repositories

--- database/src/main/java/com/iyad/repository/PaymentRepository.java
+++ database/src/main/java/com/iyad/repository/PaymentRepository.java
@@ -2,8 +2,9 @@ package com.iyad.repository;
 
 import com.iyad.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
--- database/src/main/java/com/iyad/repository/WorkerRepository.java
+++ database/src/main/java/com/iyad/repository/WorkerRepository.java
@@ -2,8 +2,9 @@ package com.iyad.repository;
 
 import com.iyad.model.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, UUID> {
 }
