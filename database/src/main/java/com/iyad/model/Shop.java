18e6aaae165fe5d49a4212312d235f07e3a1711d
IyadA <iyada@radware.com>
2025-02-03 16:24:49 +0200

    add Shop entity and its Repository

---
+++ database/src/main/java/com/iyad/model/Shop.java
@@ -0,0 +1,32 @@
+package com.iyad.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String name;
+
+    private String shopOwner;
+
+    private String phoneNumber;
+
+    private String location;
+
+    @OneToMany(mappedBy = "material", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Payment> payments;
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Material> materials = new ArrayList<>();
+}
---
+++ database/src/main/java/com/iyad/repository/ShopRepository.java
@@ -0,0 +1,14 @@
+package com.iyad.repository;
+
+import com.iyad.model.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, UUID> {
+
+    Optional findByName(String name);
+}
