6c8a010c0b6ceda631558b1530b2ccbd02f364ae
IyadA <iyada@radware.com>
2025-02-04 21:48:43 +0200

    fix the Urls

--- core/src/main/java/com/iyad/bcm/controller/PaymentController.java
+++ core/src/main/java/com/iyad/bcm/controller/PaymentController.java
@@ -33,14 +33,14 @@ public class PaymentController {
         return ResponseEntity.ok("Payment for shop created successfully");
     }
 
-    @GetMapping("/shop/{name")
+    @GetMapping("/shop/{name}")
     public ResponseEntity<List<PaymentDTO>> getPaymentsByShopName(@PathVariable String name) {
         List<PaymentDTO> dtos = paymentService.getPaymentsByShopName(name);
         return ResponseEntity.ok(dtos);
     }
 
     //Worker
-    @PostMapping("/worker/{name")
+    @PostMapping("/worker/{name}")
     public ResponseEntity<String> payForWorker(@PathVariable String name, @RequestBody PaymentDTO dto) throws Throwable {
         paymentService.payForWorker(name, dto);
         return ResponseEntity.ok("Payment for worker created successfully");
