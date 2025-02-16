fb15d0b4eeb3d99f90de00005e3a20d20cd0bc70
IyadA <iyada@radware.com>
2025-02-16 17:16:07 +0200

    fix the code formatting

--- ui/frontend/src/actions/RestRequest.js
+++ ui/frontend/src/actions/RestRequest.js
@@ -4,38 +4,36 @@ const LOGIN_PATH = "/";
 const getAuthToken = () => localStorage.getItem("token");
 
 const handleUnauthorized = (response, url) => {
-
-    if (url.includes("login")) {
-        return response.json(); // Handle login response
-    } else {
-        localStorage.removeItem("token");
-        window.location.href = LOGIN_PATH;
-        throw new Error("Unauthorized");
-    }
+  if (url.includes("login")) {
+    return response.json(); // Handle login response
+  } else {
+    localStorage.removeItem("token");
+    window.location.href = LOGIN_PATH;
+    throw new Error("Unauthorized");
+  }
 };
 
 const handleResponse = (response, url) => {
+  if (response.status === 401 || response.status === 403) {
+    return handleUnauthorized(response, url);
+  }
 
-    if (response.status === 401 || response.status === 403) {
-        return handleUnauthorized(response, url);
-    }
-
-    const contentType = response.headers.get("Content-Type") || "";
-    return contentType.includes("text") ? response.text() : response.json();
+  const contentType = response.headers.get("Content-Type") || "";
+  return contentType.includes("text") ? response.text() : response.json();
 };
 
 export const RestRequest = (url, method = "GET", body = null) => {
-    return fetch(`${BASE_URL}${url}`, {
-        method,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${getAuthToken()}`,
-        },
-        ...(body ? {body: JSON.stringify(body)} : {}),
-    })
-        .then(response => handleResponse(response, url))
-        .catch(error => {
-            console.error("API Request Failed:", error);
-            throw error;
-        });
+  return fetch(`${BASE_URL}${url}`, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAuthToken()}`,
+    },
+    ...(body ? { body: JSON.stringify(body) } : {}),
+  })
+    .then((response) => handleResponse(response, url))
+    .catch((error) => {
+      console.error("API Request Failed:", error);
+      throw error;
+    });
 };
