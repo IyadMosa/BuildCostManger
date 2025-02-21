a9ec915a49df3212392638f4c6410df2b39232f4
IyadA <iyada@radware.com>
2025-02-21 23:07:00 +0200

    add missing transactionDate field

--- database/src/main/java/com/iyad/model/BankTransferPayment.java
+++ database/src/main/java/com/iyad/model/BankTransferPayment.java
@@ -4,6 +4,8 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Entity
 @DiscriminatorValue("BANK_TRANSFER")
@@ -13,4 +15,5 @@ public class BankTransferPayment extends Payment {
     private String bankAccount;
     private String bankName;
     private String bankBranch;
+    private LocalDate transactionDate;
 }
