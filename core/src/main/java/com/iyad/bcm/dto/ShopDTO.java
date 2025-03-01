d49f8f3bb343b7dfaf807c8f1422d19cedb0bd5e
IyadA <iyada@radware.com>
2025-03-01 15:55:38 +0200

    use @Data

--- core/src/main/java/com/iyad/bcm/dto/ShopDTO.java
+++ core/src/main/java/com/iyad/bcm/dto/ShopDTO.java
@@ -11,44 +11,4 @@ public class ShopDTO {
     private String shopOwner;
     private String location;
     private String phoneNumber;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getShopOwner() {
-        return shopOwner;
-    }
-
-    public void setShopOwner(String shopOwner) {
-        this.shopOwner = shopOwner;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
