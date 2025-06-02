12c0435fdeff3f3f0407d9f1a03bb883c4c60b06
IyadA <iyada@radware.com>
2025-06-02 19:29:49 +0300

    adding test controller update

--- core/src/main/java/com/iyad/bcm/controller/TestController.java
+++ core/src/main/java/com/iyad/bcm/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String root() {
         return "API is running";
     }
